dc91e79f32743b22f9466a56eefe3709b43c3dcb
genzuby <genzuby@gmail.com>
2019-07-05 22:10:53 +0900

    up http

--- src/api/seoulOpenApi.js
+++ src/api/seoulOpenApi.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const API_KEY = "54637a4c4367656e35395a5a48566b";
 
 export default axios.create({
-  baseURL: `http://openapi.seoul.go.kr:8088/${API_KEY}/json`
+  baseURL: `//openapi.seoul.go.kr:8088/${API_KEY}/json`
 });
