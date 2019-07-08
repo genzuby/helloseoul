85dbaca1ec051fec5d11f2e494f18a4291c9136b
genzuby <genzuby@gmail.com>
2019-07-08 13:58:52 +0900

    api

--- src/api/koreaTourOpenApi.js
+++ src/api/koreaTourOpenApi.js
@@ -7,7 +7,7 @@ const tourDesc1 = (contentid, id) =>
 const tourDesc2 = (contentid, id) =>
   `https://api.visitkorea.or.kr/openapi/service/rest/EngService/detailInfo?ServiceKey=${TOUR_API_KEY}&contentTypeId=${contentid}&contentId=${id}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&listYN=Y&_type=json`;
 const tourPics = (contentid, id) =>
-  `http://api.visitkorea.or.kr/openapi/service/rest/EngService/detailImage?ServiceKey=${TOUR_API_KEY}&contentTypeId=${contentid}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&contentId=${id}&imageYN=Y&numOfRows=100&pageNo=1&_type=json`;
+  `https://api.visitkorea.or.kr/openapi/service/rest/EngService/detailImage?ServiceKey=${TOUR_API_KEY}&contentTypeId=${contentid}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&contentId=${id}&imageYN=Y&numOfRows=100&pageNo=1&_type=json`;
 
 export const TOUR_MARKET_LIST_URL = `https://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList?ServiceKey=${TOUR_API_KEY}&contentTypeId=79&areaCode=1&sigunguCode=&cat1=A04&cat2=A0401&cat3=A04010200&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=20&pageNo=1&_type=json`;
 export const tourMarketInfo = id => tourMainInfo(79, id);
@@ -20,4 +20,4 @@ export const tourWalkDesc1 = id => tourDesc1(76, id);
 export const tourWalkDesc2 = id => tourDesc2(76, id);
 export const tourWalkPic = id => tourPics(76, id);
 
-export const TOUR_EXPL_LIST_URL = `http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList?ServiceKey=${TOUR_API_KEY}&contentTypeId=76&areaCode=1&sigunguCode=&cat1=A02&cat2=A0201&cat3=&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=100&pageNo=1&_type=json`;
+export const TOUR_EXPL_LIST_URL = `https://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList?ServiceKey=${TOUR_API_KEY}&contentTypeId=76&areaCode=1&sigunguCode=&cat1=A02&cat2=A0201&cat3=&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=100&pageNo=1&_type=json`;
